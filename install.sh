a5f9509f620bd24d4f72958a697c73863b0085c4
Nikhil Khedekar <nikhilvkhedekar@gmail.com>
2017-11-07 00:38:29 +0530

    removed ENV and gym-ple

--- install.sh
+++ install.sh
@@ -1,46 +1,6 @@
 #!/bin/bash
 
-echo "Installing virtualenv"
-pip install virtualenv 
+sudo apt-get install -y python3-numpy python3-dev python3-pip cmake zlib1g-dev libjpeg-dev xvfb libav-tools xorg-dev python-opengl libboost-all-dev libsdl2-dev swig
 
-echo "Creating a virtual environment named openai"
-virtualenv ENV
+sudo pip3 install gym[all]
 
-echo "Entering Virtual ENV"
-source ./ENV/bin/activate #activate the environment openai
-
-echo "Python package installation about to start in 1.2.3"
-
-echo "Installing OpenAI GYM"
-pip install gym
-
-echo "Installing some of the know game env in Open ai"
-pip install gym['atari']
-
-
-echo "Installing System dependencies for PLE. Will require Root access"
-sudo apt-get install mercurial python-dev python-numpy python-opengl \
-    libav-tools libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev \
-    libsdl1.2-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev \
-    libtiff5-dev libx11-6 libx11-dev fluid-soundfont-gm timgm6mb-soundfont \
-    xfonts-base xfonts-100dpi xfonts-75dpi xfonts-cyrillic fontconfig fonts-freefont-ttf
-
-sudo apt-get install python-pygame
-
-echo "Installing PyGame-Learning-Environment"
-git clone https://github.com/ntasfi/PyGame-Learning-Environment.git
-cd PyGame-Learning-Environment/
-pip install -e .
-cd ..
-
-pip install ppaquette-gym-doom
-pip install pygame
-
-git clone https://github.com/lusob/gym-ple.git
-cd gym-ple/
-pip install -e .
-cd ..
-
-echo "Removing Unwanted files"
-rm -rf gym-ple
-rm -rf PyGame-Learning-Environment
