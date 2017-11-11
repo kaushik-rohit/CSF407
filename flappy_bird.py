f24bcffcd7aaad105c9248e08dbde0a03d07d672
Rohit Kaushik <kaushikrohit325@gmail.com>
2017-11-11 21:57:32 +0530

    DQN implementation for Cart-Pole & flappy bird

---
+++ flappy_bird.py
@@ -0,0 +1,68 @@
+import gym
+import gym_ple
+
+
+class DQNAgent:
+
+    def __init__(self):
+        pass
+    
+    def _build_model(self):
+        model = Sequential()
+        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse',
+                      optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+            
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])  # returns action
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                target = (reward + self.gamma *
+                          np.amax(self.model.predict(next_state)[0]))
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def load(self, name):
+        self.model.load_weights(name)
+
+    def save(self, name):
+        self.model.save_weights(name)
+
+if __name__ == '__main__':
+    
+    env = gym.make('FlappyBird-v0')
+    
+    
+    agent = RandomAgent(env.action_space)
+    
+    episode_count = 100
+    reward = 0
+    done = False
+    
+    for i in range(episode_count):
+        ob = env.reset()
+        
+        while True:
+            action = agent.act(ob, reward, done)
+            ob, reward, done, _ = env.step(action)
+            env.render()
+            
+            if(done):
+                break
